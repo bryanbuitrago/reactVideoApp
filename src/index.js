880bf66e3cce88eaedcf7278df0dcdb6d037f454
bryan buitrago <bryanbuitrago86@gmail.com>
2017-03-28 22:07:06 -0400

    imported the SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyBMDaF8i8cI2R-2J68LQuZT9ozwJNlIZMM';
 
 const App = () => {
-
-  return <div>Hi!</div>
+  return ( <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
