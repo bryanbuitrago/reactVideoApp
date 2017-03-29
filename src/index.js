06f2b01563eac23be61f90007cd700b7a979fd46
bryan buitrago <bryanbuitrago86@gmail.com>
2017-03-29 17:29:40 -0400

    imported YTSearch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBMDaF8i8cI2R-2J68LQuZT9ozwJNlIZMM';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, data => {
+  console.log(data);
+});
 
 const App = () => {
   return ( <div>
