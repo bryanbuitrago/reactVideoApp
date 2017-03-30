707f440e44e8f6e9e781afcfdf1587af2cbb61ca
bryan buitrago <bryanbuitrago86@gmail.com>
2017-03-30 10:33:38 -0400

    added video.etag as the key attribute

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,8 +2,8 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-  const videoItems = props.videos.map((video, key) => {
-    return <VideoListItem video={video} key={key}/>
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} key={video.etag}/>
   });
   return (
     <ul className="col-md-4 list-group">
