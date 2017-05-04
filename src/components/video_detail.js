d9a13d1a243952a770b9a264e19d977876e571fe
bryan buitrago <bryanbuitrago86@gmail.com>
2017-05-03 20:25:39 -0400

    Fixed a bug in the component

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
-  if (!video) {
-    return <div>Loading...</div>
+  if(!video) {
+    return <div>Loading..</div>;
   }
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
-  return(
-    <div className="video-detail col">
+  return (
+    <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe src="{url}" className="embed-responsive-item"></iframe>
+        <iframe src={url} className="embed-responsive-item"></iframe>
       </div>
       <div>{video.snippet.title}</div>
       <div>{video.snippet.description}</div>
